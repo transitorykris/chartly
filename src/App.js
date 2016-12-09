8bcba1cdeba8dbd3a2ef5ff94cb3a150c28b0fee
Kris Foster <kris.foster@gmail.com>
2016-12-08 16:24:39 -0800

    plot it all out

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <p>Chartly</p>
         <Streamer />
       </div>
     );
--- src/Streamer.js
+++ src/Streamer.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
-import { VictoryLine } from 'victory'
+import { VictoryChart, VictoryLine, VictoryAxis } from 'victory'
+
+const chartStyle = {
+  labels: {fontSize: 12},
+  parent: {border: "1px solid #ccc"},
+}
+
+const lightGreen500 = "#8BC34A";
+const deepOrange600 = "#F4511E";
+
+const posLineStyle = {
+  data: {
+    strokeWidth: 2,
+    stroke: lightGreen500,
+  },
+}
+
+const negLineStyle = {
+  data: {
+    strokeWidth: 2,
+    stroke: deepOrange600,
+  },
+}
 
 export default class Streamer extends Component {
   constructor(props) {
     super(props)
-    this.state = {data: []}
+    this.state = {
+      posData: [{time: Date.now(), score: 0}],
+      negData: [{time: Date.now(), score: 0}],
+    }
   }
   setUp() {
     var that = this
@@ -14,9 +39,15 @@ export default class Streamer extends Component {
     }
     this.logSocket.onmessage = function (msg) {
       let event = JSON.parse(msg.data)
-      let data = that.state.data
-      data.push({score: event.Score})
-      that.setState({data: data})
+      if (event.Score > 0) {
+        let data = that.state.posData
+        data.push({time: Date.now(), score: event.Score})
+        that.setState({posData: data})
+      } else {
+        let data = that.state.negData
+        data.push({time: Date.now(), score: event.Score})
+        that.setState({negData: data})
+      }
       console.log(event)
     }
     this.logSocket.onerror = function (error) {
@@ -34,7 +65,12 @@ export default class Streamer extends Component {
   }
   render() {
     return (
-      <VictoryLine data={this.state.data} x="time" y="score" />
+      <VictoryChart style={chartStyle}>
+        <VictoryLine style={posLineStyle} data={this.state.posData} x="time" y="score" />
+        <VictoryLine style={negLineStyle} data={this.state.negData} x="time" y="score" />
+        <VictoryAxis style={{tickLabels: {fontSize: 0}}} label="" tickValues={[]} />
+        <VictoryAxis dependentAxis label="Score" tickValues={[-8, -6, -4, -2, 2, 4, 6, 8]} />
+      </VictoryChart>
     )
   }
 }
